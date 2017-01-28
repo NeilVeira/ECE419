33c26bfd24660dc0d06272ef92b4f10a9e8020b9
NeilVeira <neil.veira@mail.utoronto.ca>
2017-01-28 14:14:54 -0500

    Update unit tests

--- StorageServer/src/testing/TestKVMessage.java
+++ StorageServer/src/testing/TestKVMessage.java
@@ -24,14 +24,14 @@ public class TestKVMessage extends TestCase {
 		assertEquals(message.getMsg(), "\"get\" \" \" \"key\" \"value\"");
 	}
 	
-	public void testConnectInvalid(){
-		MessageType message = new MessageType("connect","localhost","5000"," ");
-		assertEquals("Incorrect number of tokens for message put",message.error);
+	public void testDisconnectInvalid(){
+		MessageType message = new MessageType("disconnect","localhost","5000"," ");
+		assertEquals("Key and value must be empty for message disconnect",message.error);
 	}
 	
 	public void testPutInvalid(){
 		MessageType message = new MessageType("put"," ","key"," ");
-		assertEquals("Incorrect number of tokens for message put",message.error);
+		assertEquals("Key and value must not be empty for message put",message.error);
 	}
 	
 	public void testSpacesAndQuotesInStrings(){
@@ -43,51 +43,23 @@ public class TestKVMessage extends TestCase {
 		assertEquals(message.getMsg(), "\"get\" \" \" \"this is the \"\"key\"\"\" \"this is the\"\"\"\" value\"");
 	}
 	
-	//These test are out of date
-	/*public void testDisconnectInvalid(){
-		MessageType message = new MessageType("disconnect"," ","key", false);
-		assertEquals("Incorrect number of tokens for message disconnect",message.error);
-	}
-	
-	public void testConstructWithStatus(){
-		MessageType message = new MessageType("get GET_SUCCESS key1 value1", true);
-		assertNull(message.error);
-		assertEquals("get",message.getHeader());
-		assertEquals("GET_SUCCESS",message.getStatus());
-		assertEquals("key1",message.getKey());
-		assertEquals("value1",message.getValue());
-	}
-	
-	public void testConstructWithStatusInvalid(){
-		MessageType message = new MessageType("put","PUT_SUCCESS","key1"," ");
-		assertEquals("Incorrect number of tokens for message put",message.error);
-	}
-	
+
 	public void testConvertToBytes(){
-		MessageType message = new MessageType("get"," ","key1"," ");
+		MessageType message = new MessageType("get"," ","key"," ");
 		assertNull(message.error);
 		byte[] bytes = message.getMsgBytes();
-		Assert.assertArrayEquals(new byte[]{103,101,116,32,107,101,121,49,10}, bytes);
-	}
-	
-	public void testConstructFromBytesClientToServer(){
-		byte[] bytes = new byte[]{103,101,116,32,107,101,121,49}; 
-		MessageType message = new MessageType(bytes, false);
-		assertNull(message.error);
-		assertEquals("get",message.getHeader());
-		assertEquals("key1",message.getKey());
-		assertEquals("get key1",message.getMsg());
+		Assert.assertArrayEquals(new byte[]{34, 103, 101, 116, 34, 32, 34, 32, 34, 32, 34, 107, 101, 121, 34, 32, 34, 32, 34, 10}, bytes);
 	}
 	
 	public void testConstructFromBytesServerToClient(){
-		byte[] bytes = new byte[]{103,101,116,32,71,69,84,95,83,85,67,67,69,83,83,32,107,101,121,49,32,118,97,108,117,101,49,10};
-		MessageType message = new MessageType(bytes, true);
+		byte[] bytes = new byte[]{34, 112, 117, 116, 34, 32, 34, 32, 34, 32, 34, 107, 101, 121, 34, 32, 34, 118, 97, 108, 117, 101, 34, 10};
+		MessageType message = new MessageType(bytes);
 		assertTrue(message.isValid);
 		assertEquals("get",message.getHeader());
-		assertEquals("GET_SUCCESS",message.getStatus());
-		assertEquals("key1",message.getKey());
-		assertEquals("value1",message.getValue());
-		assertEquals("get GET_SUCCESS key1 value1",message.getMsg());
+		assertEquals("",message.getStatus());
+		assertEquals("key",message.getKey());
+		assertEquals("value",message.getValue());
+		assertEquals("\"get\" \" \" \"key\" \"value\"",message.getMsg());
 		
-	}*/
+	}
 }
