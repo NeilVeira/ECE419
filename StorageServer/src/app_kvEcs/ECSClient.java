6d2d59d66dfe108b8ef84e92d9b3eef9e84d9f4a
NeilVeira <neil.veira@mail.utoronto.ca>
2017-03-01 21:34:38 -0500

    Add ECSClient class

---
+++ StorageServer/src/app_kvEcs/ECSClient.java
@@ -0,0 +1,12 @@
+package app_kvEcs;
+
+public class ECSClient {
+	
+	/**
+	 * Entry point to run the ECS
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
