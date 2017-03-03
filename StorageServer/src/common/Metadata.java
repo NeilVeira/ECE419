482f3c357f386f5ef3d81b868227e29f1598247d
NeilVeira <neil.veira@mail.utoronto.ca>
2017-03-03 00:14:56 -0500

    Finish implementing metadata class

--- StorageServer/src/common/Metadata.java
+++ StorageServer/src/common/Metadata.java
@@ -3,17 +3,20 @@ package common;
 import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 import java.math.BigInteger;
+import java.util.*;
 
 /**
  * Class to manage the mapping of servers to hash ranges
- *
+ * and the hash ring. 
  */
 public class Metadata{
 	private MessageDigest hasher;
+	private TreeMap<BigInteger,Server> hashRing;
 	
 	public Metadata(){
 		try{
 			this.hasher = MessageDigest.getInstance("MD5");
+			this.hashRing = new TreeMap<BigInteger,Server>();
 		}
 		catch (Exception e){
 			e.printStackTrace();
@@ -22,30 +25,57 @@ public class Metadata{
 	
 	/**
 	 * Return a 128-bit (BigInteger) MD5 hash of the given IP address and port
-	 * @param ipAddress
-	 * @param port
-	 * @return
+	 * @return the hash value
 	 */
-	public BigInteger getHash(String ipAddress, int port){
-		hasher.update(ipAddress.getBytes());
-		byte[] portBytes = ByteBuffer.allocate(4).putInt(port).array(); //convert int to byte array
+	public BigInteger serverHash(Server server){
+		hasher.update(server.ipAddress.getBytes());
+		byte[] portBytes = ByteBuffer.allocate(4).putInt(server.port).array(); //convert int to byte array
 		hasher.update(portBytes); 
 		byte[] hash = hasher.digest();
 		hasher.reset();
 		return new BigInteger(hash);
 	}
 	
-	public void addServer(String ipAddress, int port){
-		//TODO
+	public BigInteger objectHash(String key){
+		hasher.update(key.getBytes());
+		byte[] hash = hasher.digest();
+		hasher.reset();
+		return new BigInteger(hash);
 	}
 	
-	public void removeServer(String ipAddress, int port){
-		//TODO
+	/**
+	 * Add the given server to the hash ring
+	 */
+	public void addServer(Server server){
+		BigInteger hash = serverHash(server);
+		hashRing.put(hash, server);
 	}
 	
+	/**
+	 * Remove the given server from the metadata
+	 * @param server
+	 */
+	public void removeServer(Server server){
+		BigInteger hash = serverHash(server);
+		hashRing.remove(hash);
+	}
+	
+	/**
+	 * Return the server responsible for the data key or null if the 
+	 * ring is empty. (NOT TESTED YET!)
+	 */
 	public Server getResponsible(String key){
-		//TODO
-		return new Server("",0);
+		BigInteger keyHash = objectHash(key);
+		Map.Entry<BigInteger,Server> entry = hashRing.ceilingEntry(keyHash);
+		if (entry == null){
+			//key is past the last server - wrap around to first
+			entry = hashRing.firstEntry();
+			if (entry == null){
+				//hashRing is empty
+				return null;
+			}
+		}
+		return entry.getValue();
 	}
 	
 	/**
@@ -56,7 +86,7 @@ public class Metadata{
 		public String ipAddress;
 		public int port;
 		
-		Server(String ipAddress, int port){
+		public Server(String ipAddress, int port){
 			this.ipAddress = ipAddress;
 			this.port = port;
 		}
--- StorageServer/src/testing/TestMetadata.java
+++ StorageServer/src/testing/TestMetadata.java
@@ -9,17 +9,33 @@ import java.math.BigInteger;
 import junit.framework.TestCase;
 
 public class TestMetadata extends TestCase {
+	Metadata md;
 	
 	public void setUp() {
+		this.md = new Metadata();
 	}
 
 	public void tearDown() {
 	}
 	
 	public void testHash() {
-		Metadata md = new Metadata();
-		BigInteger hash = md.getHash("localhost", 50002);
+		BigInteger hash = md.serverHash(md.new Server("localhost", 50002));
 		assertEquals(hash.toString(), "-93864682652215908080847256054918673801");
 	}
+	
+	public void testAddServer() {
+		md.addServer(md.new Server("localhost",50000));
+		md.addServer(md.new Server("127.0.0.01",50001));
+		md.addServer(md.new Server("localhost",50000)); 
+		//TODO: check correctness
+	}
+	
+	public void testRemoveServerPresent() {
+		md.addServer(md.new Server("localhost",50000));
+		md.removeServer(md.new Server("localhost",50000));
+		//TODO: check correctness
+	}
+	
+	//TODO: test getResponsible
 	
 }
